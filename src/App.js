8d61e463cc47f76bb9040d5217c049eb5e739080
Dinesh Yadav <dineshyadav74950@gmail.com>
2025-06-15 21:44:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Join from './pages/Join';
 import About from './pages/About';
 import Footer from './pages/Footer';
+
 function App() {
   return (
     <Router>
       <Header />
-      <div className="pt-20"> {/* add top padding to avoid content under navbar */}
+      <div className="pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About/>} />
-          <Route path="/join" element={<Join />} />        
-          {/* <Route path="/footer" element={<Footer />} /> */}
+          <Route path="/About" element={<About />} />
+          <Route path="/join" element={<Join />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
